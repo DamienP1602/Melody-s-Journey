1ddcff2f04d988f1d777c5088114c5b057e2ad30
DamienP1602 <148540742+DamienP1602@users.noreply.github.com>
2024-04-08 13:16:27 +0200

    continue character

--- Melody_s_Journey/Character.cpp
+++ Melody_s_Journey/Character.cpp
@@ -1,1 +1,40 @@
 #include "Character.h"
+
+Character::Character(const string& _name, const CharacterType& _type, const DominantStat& _dominantStat)
+{
+	name = _name;
+	type = _type;
+	dominantStat = _dominantStat;
+
+	Init();
+}
+
+void Character::Init()
+{
+	switch (dominantStat)
+	{
+	case DS_STRENGTH:
+		strength += 3;
+		break;
+	case DS_INTELLIGENCE:
+		intelligence += 3;
+		break;
+	case DS_AGILITY:
+		agility += 3;
+		break;
+	default:
+		break;
+	}
+}
+
+void Character::GainXP()
+{
+}
+
+void Character::LevelUp()
+{
+}
+
+void Character::CalculExperienceCap()
+{
+}
--- Melody_s_Journey/Character.h
+++ Melody_s_Journey/Character.h
@@ -24,29 +24,123 @@ enum DominantStat
 	DS_AGILITY
 };
 
+struct BasicStat
+{
+	usV maxHealth;
+	usV currentHealth;
+
+	usV strength;
+	usV intelligence;
+	usV agility;
+
+	usV damage;
+	usV speed;
+
+	BasicStat() = default;
+
+	BasicStat(usV _maxHealth, usV _currentHealth, usV _strength, usV _intelligence, usV _agility, usV _damage, usV _speed)
+	{
+		maxHealth = _maxHealth;
+		currentHealth = _currentHealth;
+		strength = _strength;
+		intelligence = _intelligence;
+		agility = _agility;
+		damage = _damage;
+		speed = _speed;
+	}
+};
+
+struct SpecialStat
+{
+	sV damageDeal;
+	sV damageReceive;
+	sV endTurnDamage;
+	sV healDeal;
+	sV healReceive;
+	usV cooldownReduction;
+
+	SpecialStat() = default;
+
+	SpecialStat(sV _damageDeal, sV _damageReceive, sV _endTurnDamage, sV _healDeal, sV _healReceive, usV _cooldownReduction)
+	{
+		damageDeal = _damageDeal;
+		damageReceive = _damageReceive;
+		endTurnDamage = _endTurnDamage;
+		healDeal = _healDeal;
+		healReceive = _healReceive;
+		cooldownReduction = _cooldownReduction;
+	}
+};
+
+struct LevelInformations
+{
+	usV level;
+	usV experience;
+	usV experienceCap;
+
+	LevelInformations() = default;
+
+	LevelInformations(usV _level, usV _experience, usV _experienceCap)
+	{
+		level = _level;
+		experience = _experience;
+		experienceCap = _experienceCap;
+	}
+};
+
 class Character
 {
 	string name = "DefaultCharacterName";
-		
-	usV maxHealth = 0;
-	usV currentHealth = 0;
+	
+	usV level = 1;
+	usV experience = 0;
+	usV experienceCap = 0;
 
-	usV strength = 0;
-	usV intelligence = 0;
-	usV agility = 0;
+	usV maxHealth = 100;
+	usV currentHealth = 100;
+
+	usV strength = 5;
+	usV intelligence = 5;
+	usV agility = 5;
 
 	usV damage = 0;
-	usV speed = 0;
+	usV speed = 10;
 
 	sV damageDeal = 0;
 	sV damageReceive = 0;
 	sV endTurnDamage = 0;
-	sV healthDeal = 0;
-	sV healthReceive = 0;
+	sV healDeal = 0;
+	sV healReceive = 0;
 	usV cooldownReduction = 0;
 
 	CharacterType type = CT_NONE;
 	DominantStat dominantStat = DS_NONE;
 
+public:
+	CharacterType GetType() const
+	{
+		return type;
+	}
+	DominantStat GetDominantStat() const
+	{
+		return dominantStat;
+	}
+	BasicStat GetBasicStat() const
+	{
+		return BasicStat(maxHealth, currentHealth, strength, intelligence, agility, damage, speed);
+	}
+	SpecialStat GetSpecialStat() const
+	{
+		return SpecialStat(damage, damageReceive, endTurnDamage, healDeal, healReceive, cooldownReduction);
+	}
+
+public:
+	Character(const string& _name,const CharacterType& _type, const DominantStat& _dominantStat);
+
+	void Init();
+
+	void GainXP();
+	void LevelUp();
+	void CalculExperienceCap();
 };
 
